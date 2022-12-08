207ceff4faf3a6e8b593083d69530bc86f5404ea
Christos Chatzisavvas <119799278+christoschatz@users.noreply.github.com>
2022-12-08 22:59:46 +0200

    Update recording_.waf.m

--- Sound Technology/Project_1/recording_.waf.m
+++ Sound Technology/Project_1/recording_.waf.m
@@ -1,15 +1,15 @@
 clc;clear all
 
-Fs=16000;%Sampling frequency in hertz
-ch=1;%Number of channels--2 options--1 (mono) or 2 (stereo)
+Fs=16000; % Sampling frequency in hertz
+ch=1; % Number of channels:1 (mono) or 2 (stereo)
 datatype='double';
-nbits=24;%8,16,or 24
-Nseconds=5; %seconds
+nbits=24; % 8,16,or 24
+Nseconds=5; % seconds
 recorder=audiorecorder(Fs,nbits,ch);
 disp('Start speaking..')
 recordblocking(recorder,Nseconds);
 disp('End of Recording.');
-%Store recorded audio signal in numeric array
+% Store recorded audio signal in numeric array
 x=getaudiodata(recorder,datatype);
-%Write audio file
+% Write audio file
 audiowrite('myvoice.wav',x,Fs);
